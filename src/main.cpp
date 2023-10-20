ac677de5beb7ba5f9d89731eb288dd2cbfbfc461
Ondrejó András <ondrejo.a@gmail.com>
2023-10-20 13:29:38 +0200

    ledstrip init

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#include <ws2812b.hpp>
+using namespace ws2812b;
+
 extern "C" void app_main() {
-    
+    auto gpio = 33;
+    auto led_count = 10;
+    LedStrip::Ptr led_strip = LedStrip::create(gpio, led_count);
 }
